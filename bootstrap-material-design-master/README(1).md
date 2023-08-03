4dfea8c2460261b81b524c3690930df3a87bf73f
pc@BITCOIN-TRADER <BITCOIN-TRADER>
2023-08-03 07:41:58 -0700

    cage~trucking

--- bootstrap-material-design-master/README(1).md
+++
@@ -1,2 +0,0 @@
-This project has been moved here:
-[mdbootstrap.com/docs/standard/](mdbootstrap.com/docs/standard/)
--- index.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-    // add padding top to show content behind navbar
-    navbar_height = document.querySelector('.navbar').offsetHeight;
-    document.body.style.paddingTop = navbar_height + 'px';
-  }); 
-  // DOMContentLoaded  end
